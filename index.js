242886d22357b64b85938f88874cb7082e978d03
GPlay97 <GPlay97@users.noreply.github.com>
2020-06-11 17:14:36 +0200

    log before restart

--- index.js
+++ index.js
@@ -124,9 +124,9 @@ app.post('/update', async (req, res, next) => {
         await execCmd('sync');
         res.sendStatus(200);
         await execCmd('sudo mount -o remount,ro /');
+        if (rollbar) rollbar.info('Server updated');
         await execCmd('pm2 flush && pm2 restart all');
         await execCmd('sudo systemctl restart pnpupdater.service');
-        if (rollbar) rollbar.info('Server updated');
     } catch (error) {
         if (rollbar) rollbar.error(error);
         next(error);
